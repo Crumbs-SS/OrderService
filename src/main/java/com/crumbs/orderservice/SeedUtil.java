d2c4fca8c92690a1057f80424bda5a6440d6a97f
elijahcbrooks <elijahcbrooks@gmail.com>
2021-07-09 06:28:05 -0500

    crum-056 feat: OrderDTO creation and refactoring of CartOrderDTO

--- src/main/java/com/crumbs/orderservice/SeedUtil.java
+++ src/main/java/com/crumbs/orderservice/SeedUtil.java
@@ -1,106 +1,143 @@
-package com.crumbs.orderservice;
-
-import com.crumbs.lib.entity.Customer;
-import com.crumbs.lib.entity.UserDetails;
-import com.crumbs.lib.repository.UserDetailsRepository;
-import com.crumbs.orderservice.entity.*;
-import com.crumbs.orderservice.repository.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
-@Component
-public class SeedUtil implements ApplicationRunner {
-    @Autowired CategoryRepository categoryRepository;
-    @Autowired RestaurantRepository restaurantRepository;
-    @Autowired MenuItemRepository menuItemRepository;
-    @Autowired RestaurantCategoryRepository restaurantCategoryRepository;
-
-    private final UserDetailsRepository userDetailsRepository;
-
-    @Autowired
-    SeedUtil(UserDetailsRepository userDetailsRepository){
-        this.userDetailsRepository = userDetailsRepository;
-    }
-
-    private void makeRestaurants(){
-        List<Category> categories = makeCategories();
-        for (int i = 0; i < 10; i++) {
-            Restaurant restaurant = Restaurant.builder()
-                    .name("Restaurant-" + i)
-                    .build();
-
-            restaurant = restaurantRepository.save(restaurant);
-            makeMenuItems(restaurant);
-
-            for (int k = 0; k < 3; k++){
-                int rand = new Random().nextInt(categories.size());
-                RestaurantCategory restaurantCategory = new RestaurantCategory(categories.get(rand), restaurant);
-                restaurantCategoryRepository.save(restaurantCategory);
-            }
-            restaurantRepository.save(restaurant);
-        }
-    }
-
-    private void makeMenuItems(Restaurant restaurant){
-        for (int i = 0; i < 10; i++){
-            BigDecimal bd = BigDecimal.valueOf((i + 1F) * (float) Math.random() + 3)
-                    .setScale(2, RoundingMode.HALF_UP);
-            Float price = bd.floatValue();
-
-            MenuItem menuItem = MenuItem.builder()
-                    .name("MenuItem-"+i)
-                    .price(price)
-                    .description("Menu Item for a restaurant")
-                    .build();
-
-            menuItem.setRestaurant(restaurant);
-            menuItemRepository.save(menuItem);
-            restaurantRepository.save(restaurant);
-        }
-    }
-    
-    private List<Category> makeCategories(){
-        Category burger = Category.builder().name("burger").build();
-        Category wings = Category.builder().name("wings").build();
-        Category chicken = Category.builder().name("chicken").build();
-        Category chinese = Category.builder().name("chinese").build();
-        Category seafood = Category.builder().name("seafood").build();
-        Category vegan = Category.builder().name("vegan").build();
-
-        categoryRepository.save(burger);
-        categoryRepository.save(wings);
-        categoryRepository.save(chicken);
-        categoryRepository.save(chinese);
-        categoryRepository.save(seafood);
-        categoryRepository.save(vegan);
-
-        return Arrays.asList(burger, wings, chicken, chinese, seafood, vegan);
-    }
-
-    private void makeCustomer(){
-        UserDetails user = UserDetails.builder()
-                .email("1@1.com")
-                .firstName("1")
-                .lastName("1")
-                .username("hehe")
-                .password("hehehe")
-                .build();
-        Customer customer = Customer.builder().userDetails(user).build();
-        user.setCustomer(customer);
-        userDetailsRepository.save(user);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        makeRestaurants();
-        makeCustomer();
-    }
-}
+//package com.crumbs.orderservice;
+//
+//import com.crumbs.lib.entity.*;
+//import com.crumbs.lib.repository.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.math.BigDecimal;
+//import java.math.RoundingMode;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Random;
+//
+//@Component
+//public class SeedUtil implements ApplicationRunner {
+//
+//    private final UserDetailsRepository userDetailsRepository;
+//    private final MenuItemRepository menuItemRepository;
+//    private final LocationRepository locationRepository;
+//    private final RestaurantCategoryRepository restaurantCategoryRepository;
+//    private final RestaurantRepository restaurantRepository;
+//    private final CategoryRepository categoryRepository;
+//
+//
+//    SeedUtil(UserDetailsRepository userDetailsRepository,
+//             MenuItemRepository menuItemRepository,
+//             LocationRepository locationRepository,
+//             RestaurantCategoryRepository restaurantCategoryRepository,
+//             RestaurantRepository restaurantRepository,
+//             CategoryRepository categoryRepository){
+//
+//        this.userDetailsRepository = userDetailsRepository;
+//        this.menuItemRepository = menuItemRepository;
+//        this.locationRepository = locationRepository;
+//        this.restaurantCategoryRepository = restaurantCategoryRepository;
+//        this.restaurantRepository = restaurantRepository;
+//        this.categoryRepository = categoryRepository;
+//    }
+//
+//    private void makeRestaurants(UserDetails user){
+//        List<Category> categories = makeCategories();
+//        for (int i = 0; i < 10; i++) {
+//            Location location = Location.builder()
+//                    .street(i + "- Lane")
+//                    .city(i + "- City")
+//                    .state(i + "- State")
+//                    .zipCode("12345")
+//                    .build();
+//
+//
+//            locationRepository.save(location);
+//            Restaurant restaurant = Restaurant.builder()
+//                    .name("Restaurant-" + i)
+//                    .restaurantOwner(user.getOwner())
+//                    .location(location)
+//                    .restaurantStatus(RestaurantStatus.builder().status("REGISTERED").build())
+//                    .build();
+//
+//            restaurant = restaurantRepository.save(restaurant);
+//            makeMenuItems(restaurant);
+//
+//            for (int k = 0; k < 3; k++){
+//                int rand = new Random().nextInt(categories.size());
+//                RestaurantCategory restaurantCategory = new RestaurantCategory(categories.get(rand), restaurant);
+//                restaurantCategoryRepository.save(restaurantCategory);
+//            }
+//            restaurantRepository.save(restaurant);
+//        }
+//    }
+//
+//    private void makeMenuItems(Restaurant restaurant){
+//        for (int i = 0; i < 10; i++){
+//            BigDecimal bd = BigDecimal.valueOf((i + 1F) * (float) Math.random() + 3)
+//                    .setScale(2, RoundingMode.HALF_UP);
+//            Float price = bd.floatValue();
+//
+//            MenuItem menuItem = MenuItem.builder()
+//                    .name("MenuItem-"+i)
+//                    .price(price)
+//                    .description("Menu Item for a restaurant")
+//                    .build();
+//
+//            menuItem.setRestaurant(restaurant);
+//            menuItemRepository.save(menuItem);
+//            restaurantRepository.save(restaurant);
+//        }
+//    }
+//
+//    private List<Category> makeCategories(){
+//        Category burger = Category.builder().name("burger").build();
+//        Category wings = Category.builder().name("wings").build();
+//        Category chicken = Category.builder().name("chicken").build();
+//        Category chinese = Category.builder().name("chinese").build();
+//        Category seafood = Category.builder().name("seafood").build();
+//        Category vegan = Category.builder().name("vegan").build();
+//
+//        categoryRepository.save(burger);
+//        categoryRepository.save(wings);
+//        categoryRepository.save(chicken);
+//        categoryRepository.save(chinese);
+//        categoryRepository.save(seafood);
+//        categoryRepository.save(vegan);
+//
+//        return Arrays.asList(burger, wings, chicken, chinese, seafood, vegan);
+//    }
+//
+//    private UserDetails makeCustomer(){
+//        UserDetails user = UserDetails.builder()
+//                .email("0@0.com")
+//                .firstName("0")
+//                .lastName("0")
+//                .username("user0")
+//                .password("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh")
+//                .phone("1122334455")
+//                .build();
+//
+//        UserStatus userStatus = UserStatus.builder().status("REGISTERED").build();
+//
+//        Customer customer = Customer.builder().userDetails(user).userStatus(userStatus).build();
+//        Owner owner = Owner.builder().userDetails(user).userStatus(userStatus).build();
+//
+//        user.setOwner(owner);
+//        user.setCustomer(customer);
+//
+//        userDetailsRepository.save(user);
+//
+//        return user;
+//    }
+//
+//    public void dropSchema(){
+//        categoryRepository.deleteAll();
+//        userDetailsRepository.deleteAll();
+//    }
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        dropSchema();
+//        UserDetails user = makeCustomer();
+//        makeRestaurants(user);
+//    }
+//}
--- src/main/java/com/crumbs/orderservice/service/OrderService.java
+++ src/main/java/com/crumbs/orderservice/service/OrderService.java
@@ -1,34 +1,56 @@
 package com.crumbs.orderservice.service;
 
-import com.crumbs.lib.repository.UserDetailsRepository;
+import com.crumbs.lib.entity.*;
+import com.crumbs.lib.repository.*;
 import com.crumbs.orderservice.DTO.CartItemDTO;
+import com.crumbs.orderservice.DTO.CartOrderDTO;
 import com.crumbs.orderservice.DTO.OrderDTO;
 import com.crumbs.orderservice.DTO.OrdersDTO;
-import com.crumbs.orderservice.entity.*;
 import com.crumbs.orderservice.mapper.FoodOrderMapper;
-import com.crumbs.orderservice.repository.*;
+import com.crumbs.orderservice.mapper.OrderDTOMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.*;
 
 @Service
 @Transactional(rollbackFor = { Exception.class })
 public class OrderService {
 
-    @Autowired OrderRepository orderRepository;
-    @Autowired FoodOrderRepository foodOrderRepository;
-    @Autowired MenuItemRepository menuItemRepository;
-    @Autowired RestaurantRepository restaurantRepository;
-    @Autowired FoodOrderMapper foodOrderMapper;
-    @Autowired UserDetailsRepository userDetailsRepository;
+    private final OrderRepository orderRepository;
+    private final FoodOrderRepository foodOrderRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final FoodOrderMapper foodOrderMapper;
+    private final UserDetailsRepository userDetailsRepository;
+    private final LocationRepository locationRepository;
+    private final OrderDTOMapper orderDTOMapper;
 
-    public List<Order> createOrder(Integer userId, OrderDTO orderDTO){
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    OrderService(OrderRepository orderRepository,
+            FoodOrderRepository foodOrderRepository,
+            RestaurantRepository restaurantRepository,
+            FoodOrderMapper foodOrderMapper,
+            LocationRepository locationRepository,
+            UserDetailsRepository userDetailsRepository,
+            OrderDTOMapper orderDTOMapper){
+
+        this.orderRepository = orderRepository;
+        this.foodOrderRepository = foodOrderRepository;
+        this.restaurantRepository = restaurantRepository;
+        this.foodOrderMapper = foodOrderMapper;
+        this.userDetailsRepository = userDetailsRepository;
+        this.locationRepository = locationRepository;
+        this.orderDTOMapper = orderDTOMapper;
+
+    }
+
+    public List<Order> createOrder(Long userId, CartOrderDTO cartOrderDTO){
 
         List<Order> ordersCreated = new ArrayList<>();
         UserDetails user = userDetailsRepository.findById(userId).orElseThrow();
-        List<CartItemDTO> cartItems = orderDTO.getCartItems();
+        List<CartItemDTO> cartItems = cartOrderDTO.getCartItems();
         List<FoodOrder> foodOrders = foodOrderMapper.getFoodOrders(cartItems);
         Map<Long, List<FoodOrder>> hashMap = createHashMap(foodOrders);
 
@@ -36,44 +58,51 @@ public class OrderService {
             Restaurant restaurant = restaurantRepository.findById(restaurantId)
                     .orElseThrow();
 
+            Location deliverLocation = Location.builder()
+                    .zipCode("11111")
+                    .state("Texas")
+                    .city("Houston")
+                    .street(cartOrderDTO.getAddress())
+                    .build();
+
+            locationRepository.save(deliverLocation);
             Order order = Order.builder()
                     .restaurant(restaurant)
                     .customer(user.getCustomer())
-                    .fulfilled(false)
+                    .orderStatus(OrderStatus.builder().status("AWAITING_DRIVER").build())
                     .foodOrders(foodOrdersList)
-                    .preferences(orderDTO.getPreferences())
-                    .phone(orderDTO.getPhone())
-                    .address(orderDTO.getAddress())
+                    .preferences(cartOrderDTO.getPreferences())
+                    .phone(cartOrderDTO.getPhone())
+                    .createdAt(new Timestamp(new Date().getTime()))
+                    .deliveryTime(new Timestamp(new Date().getTime()))
+                    .deliveryLocation(deliverLocation)
                     .build();
+            foodOrdersList.forEach(foodOrder -> foodOrder.setOrder(order));
 
             orderRepository.save(order);
             ordersCreated.add(order);
 
-            foodOrdersList.forEach(foodOrder -> {
-                foodOrder.setOrder(order);
-                foodOrderRepository.save(foodOrder);
-            });
         });
 
         return ordersCreated;
     }
 
-    public OrdersDTO getOrders(Integer userId){
+    public OrdersDTO getOrders(Long userId){
         UserDetails user = userDetailsRepository.findById(userId).orElseThrow();
-
         return OrdersDTO.builder()
                 .orders(user.getCustomer().getOrders())
-                .activeOrders(getOrders(userId, false))
-                .inactiveOrders(getOrders(userId, true))
+                .activeOrders(getOrders(userId, "AWAITING_DRIVER"))
+                .inactiveOrders(getOrders(userId, "FULFILLED"))
                 .build();
     }
 
-    private List<Order> getOrders(Integer userId, Boolean fulfilled){
+    private List<Order> getOrders(Long userId, String status){
         UserDetails user = userDetailsRepository.findById(userId).orElseThrow();
-        return orderRepository.findOrderByFulfilledAndCustomer(fulfilled, user.getCustomer());
+        OrderStatus orderStatus = OrderStatus.builder().status(status).build();
+
+        return orderRepository.findOrderByOrderStatusAndCustomer(orderStatus, user.getCustomer());
     }
 
-    // Key, Value pair object
     // Expected: RestaurantId: List<FoodOrder>
     private Map<Long, List<FoodOrder>> createHashMap(List<FoodOrder> foodOrders){
         Map<Long, List<FoodOrder>> hashMap = new HashMap<>();
@@ -93,4 +122,27 @@ public class OrderService {
 
         return hashMap;
     }
+
+    public OrderDTO updateOrder(CartOrderDTO cartOrderDTO, Long userId, Long orderId){
+        Order order = orderRepository.findById(orderId).orElseThrow();
+
+        order.setPhone(cartOrderDTO.getPhone());
+        order.setPreferences(cartOrderDTO.getPreferences());
+        order.getDeliveryLocation().setStreet(cartOrderDTO.getAddress());
+
+        if(cartOrderDTO.getCartItems() != null){
+            //Remove old foodOrders
+            order.getFoodOrders().forEach(foodOrderRepository::delete);
+
+            // Create FoodOrder for each cartItem from the orderDto
+            List<FoodOrder> foodOrders = foodOrderMapper.getFoodOrders(cartOrderDTO.getCartItems());
+            foodOrders.forEach(foodOrder -> foodOrder.setOrder(order));
+
+            order.setFoodOrders(foodOrders);
+        }
+        orderRepository.save(order);
+
+        return orderDTOMapper.getOrderDTO(order);
+    }
+
 }
